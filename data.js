3c835ae264a74896e3326e430ee14f7bca894973
actuarynate <40470895+actuarynate@users.noreply.github.com>
2026-03-12 08:26:43 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,523 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4431887309199505,
+          "pitch": -0.16720022987923855,
+          "rotation": 0,
+          "target": "8-l2-3"
+        },
+        {
+          "yaw": 1.7772047231132806,
+          "pitch": 0.3111655529312749,
+          "rotation": 3.141592653589793,
+          "target": "1-l1a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.541498119434932,
+          "pitch": 0.008399260570005396,
+          "title": "gameroom",
+          "text": "escape"
+        }
+      ]
+    },
+    {
+      "id": "1-l1a",
+      "name": "L1A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3477.5,
+      "initialViewParameters": {
+        "yaw": 2.230192630224334,
+        "pitch": 0.1729859342509208,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.534669231788584,
+          "pitch": 0.034348283834562565,
+          "rotation": 0,
+          "target": "2-l1b"
+        },
+        {
+          "yaw": 2.6731271498292193,
+          "pitch": 0.037590338932410816,
+          "rotation": 0,
+          "target": "3-l1c"
+        },
+        {
+          "yaw": 0.6787615465786061,
+          "pitch": 0.031958448248461835,
+          "rotation": 4.71238898038469,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7351311734179777,
+          "pitch": -0.16732983731237638,
+          "title": "game3",
+          "text": "survey"
+        }
+      ]
+    },
+    {
+      "id": "2-l1b",
+      "name": "L1B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3471,
+      "initialViewParameters": {
+        "yaw": 0.40650362585258293,
+        "pitch": 0.08742043715511727,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9051146093003837,
+          "pitch": 0.14066885303440557,
+          "rotation": 0,
+          "target": "3-l1c"
+        },
+        {
+          "yaw": -2.9502281740818823,
+          "pitch": 0.09005653164532745,
+          "rotation": 0,
+          "target": "1-l1a"
+        },
+        {
+          "yaw": -2.522518501338368,
+          "pitch": 0.06272597815804914,
+          "rotation": 1.5707963267948966,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.18753451662739806,
+          "pitch": -0.1977461255979307,
+          "title": "Game",
+          "text": "influence"
+        }
+      ]
+    },
+    {
+      "id": "3-l1c",
+      "name": "L1C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3480.5,
+      "initialViewParameters": {
+        "yaw": 0.10764103047850782,
+        "pitch": 0.038089419763100096,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7397729408508233,
+          "pitch": 0.028053384024547157,
+          "rotation": 0,
+          "target": "2-l1b"
+        },
+        {
+          "yaw": 2.770137323393125,
+          "pitch": 0.024611057055770047,
+          "rotation": 0,
+          "target": "1-l1a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3215025494171204,
+          "pitch": 0.08253714981052518,
+          "title": "SpaceInvaders",
+          "text": "Game 1"
+        }
+      ]
+    },
+    {
+      "id": "4-l2-0",
+      "name": "L2-0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3479.5,
+      "initialViewParameters": {
+        "yaw": -1.772422989116869,
+        "pitch": 0.003360942381743115,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8637951776880435,
+          "pitch": 0.17185590445128796,
+          "rotation": 0,
+          "target": "5-l2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.097689758126414,
+          "pitch": -0.037370940561761046,
+          "title": "gametime",
+          "text": "go here"
+        }
+      ]
+    },
+    {
+      "id": "5-l2-1",
+      "name": "L2-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3472.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7668453160064876,
+          "pitch": 0.12712957083678234,
+          "rotation": 0,
+          "target": "4-l2-0"
+        },
+        {
+          "yaw": -2.548107685868592,
+          "pitch": 0.11453884243172396,
+          "rotation": 0,
+          "target": "6-l2-2"
+        },
+        {
+          "yaw": -1.588370430055571,
+          "pitch": 0.1425432105068989,
+          "rotation": 0,
+          "target": "8-l2-3"
+        },
+        {
+          "yaw": -1.4108776422211662,
+          "pitch": 0.052366310895195056,
+          "rotation": 1.5707963267948966,
+          "target": "7-room-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.024186084231601512,
+          "pitch": -0.001330950359772487,
+          "title": "gameroom",
+          "text": "no entry"
+        }
+      ]
+    },
+    {
+      "id": "6-l2-2",
+      "name": "L2-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3486,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8456618395944737,
+          "pitch": 0.1621959322133204,
+          "rotation": 0,
+          "target": "8-l2-3"
+        },
+        {
+          "yaw": 0.6435536848868075,
+          "pitch": 0.16109473953414444,
+          "rotation": 0,
+          "target": "5-l2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1158896500040534,
+          "pitch": 0.3094362976329812,
+          "title": "pi",
+          "text": "numbersgame"
+        },
+        {
+          "yaw": -2.4941806855886774,
+          "pitch": -0.0004936532475774413,
+          "title": "emotion",
+          "text": "game"
+        }
+      ]
+    },
+    {
+      "id": "7-room-1",
+      "name": "room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3480,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3570664256285436,
+          "pitch": 0.018722453277852935,
+          "rotation": 0,
+          "target": "8-l2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-l2-3",
+      "name": "L2-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3476.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6803490674670636,
+          "pitch": 0.07392891575373106,
+          "rotation": 0,
+          "target": "6-l2-2"
+        },
+        {
+          "yaw": 1.1849270355558055,
+          "pitch": 0.012607083120336426,
+          "rotation": 4.71238898038469,
+          "target": "7-room-1"
+        },
+        {
+          "yaw": 1.4918080781915775,
+          "pitch": 0.032642437349085895,
+          "rotation": 0,
+          "target": "5-l2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.487374039626495,
+          "pitch": 0.12463297732812784,
+          "title": "another game",
+          "text": "goes here"
+        }
+      ]
+    }
+  ],
+  "name": "Omni1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
